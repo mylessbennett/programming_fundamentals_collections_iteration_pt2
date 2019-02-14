f51bfdc2ade6cd8d6689206bc8d83c6d0b1e1e46
Myles Bennett <myles.bennett@hotmail.com>
2019-02-14 08:58:41 -0500

    completed exercise 9 parts 1-3

---
+++ exercise9.py
@@ -0,0 +1,29 @@
+grocery_list = ["bread", "apples", "milk", 'salmon', 'bananas']
+#---------------------------------------------------------------
+# for food in grocery_list:
+#     print("* {}".format(food))
+#---------------------------------------------------------------
+grocery_list.append("rice")
+#---------------------------------------------------------------
+def print_grocery_list(grocery_list):
+    for food in grocery_list:
+        print("* {}".format(food))
+    return
+#---------------------------------------------------------------
+print_grocery_list(grocery_list)
+#---------------------------------------------------------------
+print(len(grocery_list))
+#---------------------------------------------------------------
+def check_for_bananas(grocery_list):
+    bananas = False
+    for food in grocery_list:
+        if food == "bananas":
+            bananas = True
+
+    if bananas == True:
+        print("You don't need to pick up bananas today")
+    else:
+        print("You need to pick up bananas!")
+    return
+
+check_for_bananas(grocery_list)
